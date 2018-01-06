3eb620018fa4ccc0c1c7603ffa8e166a277a46dd
smorvan <morvan.sebastien@gmail.com>
2018-01-06 15:18:30 +0100

    Ajout de calib-servvo

---
+++ calib/calib-servo/calib-servo.ino
@@ -0,0 +1,41 @@
+// Issu du document http://eskimon.fr/public/ebook/Arduino_-_Premiers_pas_en_informatique_embarquee.pdf
+
+#include <Servo.h>
+int temps=1500; //censéêtre àmi­cheminentre 1000 et 2000,un bonpointdedépart
+Servo monServo;
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Calibrage Servo");
+  Serial.println("a= +10; q= -10; z= +100; s= -100");
+  monServo.attach(12);
+  //on démarreà unevaleurcenséêtrelamoitié de
+  //l'excursion totaledel'angleréalisépar le servomoteur
+  monServo.writeMicroseconds(temps);
+}
+void loop()
+{
+  //desdonnées surlaliaisonsérie ?(lorsque l'onappuiesur 'a'ou 'd')
+  if(Serial.available())
+  {
+    char commande=Serial.read(); //onlit
+    //on modifie la consigne si c'est un caractère qui nous intéresse
+    switch(commande){
+      case 'a': temps +=10;  // ajout   de  10 µs au temps
+                break;
+      case 'z': temps +=100; // ajout   de 100 µs au temps
+                break;
+      case 'q': temps -=10;  // retrait de  10 µs au temps
+                break;
+      case 's': temps -=100; // retrait de 100 µs au temps
+                break;
+                
+    }
+    // on modifielaconsigneduservo
+    monServo.writeMicroseconds(temps);
+
+    // et on faitunretour surlaconsolepour savoir oùonestrendu
+    Serial.print("Consigne courante: ");Serial.println(temps);
+  }
+}
