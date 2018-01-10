e37ff86dba0cb4409af62753b76d7ef9900ffac3
smorvan <smorvan>
2018-01-10 17:19:25 +0100

    Mise en forme de Mini et ajout d'un delai après animation

--- mini/mini.ino
+++ mini/mini.ino
@@ -1,76 +1,106 @@
-// fleur mini 
+// 
+//  ,---.    ,---..-./`) ,---.   .--..-./`)  
+//  |    \  /    |\ .-.')|    \  |  |\ .-.') 
+//  |  ,  \/  ,  |/ `-' \|  ,  \ |  |/ `-' \ 
+//  |  |\_   /|  | `-'`"`|  |\_ \|  | `-'`"` 
+//  |  _( )_/ |  | .---. |  _( )_\  | .---.  
+//  | (_ o _) |  | |   | | (_ o _)  | |   |  
+//  |  (_,_)  |  | |   | |  (_,_)\  | |   |  
+//  |  |      |  | |   | |  |    |  | |   |  
+//  '--'      '--' '---' '--'    '--' '---'  
+//                                       
+
 // 2 servos - 1 Module de capteur infrarouge d'évitement d'obstacles (https://e.banana-pi.fr/distance/121-module-de-capteur-infrarouge-d-evitement-d-obstacles.html)
 
-
-//Scenario :
-//elle s'oriente vers la personne 
-//et s'ouvre et se ferme 3x 
-//puis de se remet en position "normal"
-
+// Scenario :
+// - elle s'oriente vers la personne 
+// - s'ouvre et se ferme 3x 
+// - puis de se remet en position initiale
 
 #include <Servo.h>
 
-int led = 2;          //led temoin
-int inputPin = 8;    //port entréé
-int  val;         //variable pour l'etat du cpateur
+// Led temoin
+int ledTemoin = 2; 
+         
+// Capteur
+int inputCapteur = 8;
 
-Servo myservo2;  // servos
-Servo myservo1;
+// Servos
+Servo servoOrientation;  
+Servo servoPetales;
 
+// parcourtServo : passe le servo concerné, le debut et la fin.
+// Passe le nom du servo vers l'object servo a travers &monservo
+void parcourtServo( Servo &monservo, int depart, int fin, int vitesse) {
+ 
+  //if (vitesse <=1 ) {vitesse == 1; }
+  if (depart > fin)
+  {
+    for (int pos = depart ; pos >= fin ; pos--) 
+    { 
+      monservo.write(pos);
+      delay(vitesse);
+    }
+  }
+  else
+  {
+    for (int pos = depart ; pos <= fin ; pos++) 
+    { 
+      monservo.write(pos);
+      delay(vitesse);
+    }
+  } 
+}
 
 void  setup()
 {
-pinMode(led,OUTPUT);                    // le pin de la led est OUTPUT 
-digitalWrite(2,HIGH);                // allume la led temoin 
-pinMode(inputPin,INPUT);              // le pin du capteur est INPUt
- myservo1.attach(11);                //servo des petales qui s'ouvre et qui se ferme
- myservo2.attach(10);               // servo de la tete qui se penche vers la personne
-Serial.begin(9600);
+  // Le pin de la led est OUTPUT
+  pinMode(ledTemoin, OUTPUT);
+  // Allume la led temoin 
+  digitalWrite(ledTemoin, HIGH);
+  
+  // Le pin du capteur est INPUT
+  pinMode(inputCapteur,INPUT);
+
+  // Servo des petales qui s'ouvre et qui se ferme
+  servoPetales.attach(11);
+  
+  // Servo de la tete qui se penche vers la personne                
+  servoOrientation.attach(10);
+
+  // Initialise la liaison série pour debug
+  Serial.begin(9600);
 }
+
 void  loop()
 {
-{ val=digitalRead(inputPin);       // lit la valeur digital et la place dans la variable val 
-Serial.print(val);
+  // Lit la valeur digital et la place dans la variable val
+  int val = digitalRead(inputCapteur);
+  Serial.print("val = ");Serial.println(val);
 
-if(val==HIGH)                       //quand le capteur detecte un mouvement la varible se met HIGH
-{
-                                    // execute le programe
-Functservo(myservo2,1500,900,5);   //elle se dirirge vers la personne 
- for (int i = 0; i < 3; i++) {      //et s'ouvre et se ferme 3x 
-Functservo(myservo1,1200,1600,5);
-Functservo(myservo1,1600,1200,2);
- }
-Functservo(myservo2,900,1500,5);    //puis de se remet en position "normal"
-}
-
-}
-}
-
-
-
-void Functservo( Servo &monservo, int depart,int fin, int vitesse) {
-  //Functservo: passe en premier le servo concerné, le debut et la fin.
-  // Passe le nom du servo vers l'object servo a travers &monservo
- 
-  
-  //if (vitesse <=1 ) {vitesse == 1; }
-  if (depart > fin){
-      
-      for (int pos = depart ; pos >= fin ; pos--) 
-        { 
-         monservo.write(pos);
-         delay(vitesse);
-         //Serial.println(pos);
-        }
-  }
-   else
+  // Quand le capteur detecte un mouvement => la varible se met HIGH
+  if (val == HIGH)                       
   {
-        for (int pos = depart ; pos <= fin ; pos++) 
-        { 
-         monservo.write(pos);
-         delay(vitesse);
-         //Serial.println(pos);
-        }
+    Serial.println("Animation !");
+    // Execute le programme :
+    // - elle se dirige vers la personne
+    Serial.println("Révérence");
+    parcourtServo(servoOrientation, 1500, 900, 5);
+    
+    // - elle s'ouvre et se ferme 3x 
+    for (int i = 0; i < 3; i++) {
+      Serial.println("Ouverture pétales");
+      parcourtServo(servoPetales, 1200, 1600, 5);
+      Serial.println("Fermeture pétales");
+      parcourtServo(servoPetales, 1600, 1200, 2);
+    }
+    
+    // - puis elle se remet en position initiale
+    Serial.println("Retour en position initiale");
+    parcourtServo(servoOrientation, 900, 1500, 5);
+
+    // - Attente pour s'économiser un peu qd qq active le capteur
+    delay(5000);
   }
- 
 }
+
