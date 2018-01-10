35f1b323cb242a41805f237fcc6f418c11ad0a39
smorvan <smorvan>
2018-01-10 17:48:44 +0100

    Formatage du code et ajout d'un délai après animation

--- mini_diva/mini_diva.ino
+++ mini_diva/mini_diva.ino
@@ -1,89 +1,115 @@
-// fleur Diva(dans la continuité des kits mini)
-// 1 servos - un module enregistreur/haut parleur ISD 1820-  1a capteurs de mouvement 
+//
+//  ,---.    ,---..-./`) ,---.   .--..-./`)          ______     .-./`) ,---.  ,---.   ____
+//  |    \  /    |\ .-.')|    \  |  |\ .-.')        |    _ `''. \ .-.')|   /  |   | .'  __ `.
+//  |  ,  \/  ,  |/ `-' \|  ,  \ |  |/ `-' \        | _ | ) _  \/ `-' \|  |   |  .'/   '  \  \ 
+//  |  |\_   /|  | `-'`"`|  |\_ \|  | `-'`"`        |( ''_'  ) | `-'`"`|  | _ |  | |___|  /  |
+//  |  _( )_/ |  | .---. |  _( )_\  | .---.         | . (_) `. | .---. |  _( )_  |    _.-`   |
+//  | (_ o _) |  | |   | | (_ o _)  | |   |         |(_    ._) ' |   | \ (_ o._) / .'   _    |
+//  |  (_,_)  |  | |   | |  (_,_)\  | |   |         |  (_.\.' /  |   |  \ (_,_) /  |  _( )_  |
+//  |  |      |  | |   | |  |    |  | |   |         |       .'   |   |   \     /   \ (_ o _) /
+//  '--'      '--' '---' '--'    '--' '---'         '-----'`     '---'    `---`     '.(_,_).'
+//
 
+// Fleur Mini Diva (dans la continuité des kits mini)
+// 1 servos - un module enregistreur/haut parleur ISD 1820-  1a capteurs de mouvement
 
-//Scenario:
-//elle detecte un mouvement 
-//la fleur s'ouvre et "chante" 
-//puis de se remet en position "normal"
-
+// Scenario :
+// - elle detecte un mouvement
+// - la fleur s'ouvre et "chante"
+// - puis de se remet en position initiale
 
 #include <Servo.h>
 
-
-int inputPin = 8;  //port entré du capteur
-int command = 7;   //commande pour faire lecture sur le module de son
+// Led temoin
 int ledTemoin = 2;
-int  val;         //variable pour l'etat du capteur
 
-Servo myservo1;
+// Capteur
+int inputPin = 8;
 
+// Commande pour faire lecture sur le module de son
+int commandeSon = 7;
 
-void  setup()
-{
- 
-  pinMode(inputPin, INPUT);     // le pin du capteur est INPUT
-  pinMode(7 , OUTPUT);          // pin branché sur le module qui declanche la musique
-  digitalWrite(7,LOW);
-  pinMode(2,OUTPUT);
-  digitalWrite(2,HIGH);
-  myservo1.attach(11);        //servo des petales qui s'ouvre et qui se ferme
-  Serial.begin(9600);
-}
-void  loop()
-{
-  { val = digitalRead(inputPin);     // lit la valeur digital et la place dans la variable val
-    Serial.println(val);
+// Servo
+Servo servoPetales;
 
-    if (val == HIGH)                    //quand le capteur detecte un mouvement la varible se met HIGH
-    {
-     
-      digitalWrite(command, HIGH);       // lecture
-      delay(10);
-      Functservo(myservo1, 1100, 1600, 5);
-      delay(7000);                            // le temps d'un son
-      digitalWrite(command, LOW);
-      Functservo(myservo1, 1600, 1100, 5);
-
-
-
-
-
-    }
-    else
-    {
-      digitalWrite(command, LOW);              //sinon rien enfin position normal led temoin etainte
-
-    }
-  }
-}
-
-
-
-void Functservo( Servo &monservo, int depart, int fin, int vitesse) {
-  //Functservo: passe en premier le servo concerné, le debut et la fin.
-  // Passe le nom du servo vers l'object servo a travers &monservo
-
+// parcourtServo : passe le servo concerné, le debut et la fin.
+// Passe le nom du servo vers l'object servo a travers &servo
+void parcourtServo( Servo &servo, int depart, int fin, int vitesse) {
 
   //if (vitesse <=1 ) {vitesse == 1; }
-  if (depart > fin) {
-
+  if (depart > fin)
+  {
     for (int pos = depart ; pos >= fin ; pos--)
     {
-      monservo.write(pos);
+      servo.write(pos);
       delay(vitesse);
-      //Serial.println(pos);
     }
   }
   else
   {
     for (int pos = depart ; pos <= fin ; pos++)
     {
-      monservo.write(pos);
+      servo.write(pos);
       delay(vitesse);
-      //Serial.println(pos);
     }
   }
+}
 
+void  setup()
+{
+ // Le pin de la led est OUTPUT
+  pinMode(ledTemoin, OUTPUT);
+  // Allume la led temoin 
+  digitalWrite(ledTemoin, HIGH);
+  
+  // Le pin du capteur est INPUT
+  pinMode(inputPin, INPUT);
+  
+  // Le pin du module qui declanche la musique
+  pinMode(commandeSon , OUTPUT);
+  // Son désactivé
+  digitalWrite(commandeSon, LOW);
+  
+  // Servo des pétales qui s'ouvre et qui se ferme
+  servoPetales.attach(11);        
+
+  // Initialise la liaison série pour debug
+  Serial.begin(9600);
+}
+
+void  loop()
+{
+  // lit la valeur digital et la place dans la variable val
+  int val = digitalRead(inputPin);     
+  Serial.print("val = ");Serial.println(val);
+
+  // Quand le capteur detecte un mouvement la varaible se met HIGH
+  if (val == HIGH)                    
+  {
+    // Declenche le son
+    Serial.println("Declenche le son");
+    digitalWrite(commandeSon, HIGH);
+    delay(10);
+
+    // Ouverture des pétales
+    parcourtServo(servoPetales, 1100, 1600, 5);
+    
+    // Attend 7 secondes, la durée du son
+    delay(7000);
+
+    // Arrête le son
+    digitalWrite(commandeSon, LOW);
+
+    // Fermeture des pétales
+    parcourtServo(servoPetales, 1600, 1100, 5);
+
+    // - Attente pour s'économiser un peu qd qq active le capteur
+    delay(5000);
+  }
+  else
+  {
+    // Sinon rien enfin position normal led temoin etainte
+    digitalWrite(commandeSon, LOW);
+  }
 }
 
