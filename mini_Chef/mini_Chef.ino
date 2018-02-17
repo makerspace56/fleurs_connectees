8c18060f0fa6e32aa5de23269ad6aaf984a282a9
dubOy <mduboy@neuf.fr>
2018-02-17 21:27:35 +0100

    17/02/18

--- mini_Chef/mini_Chef.ino
+++ mini_Chef/mini_Chef.ino
@@ -22,11 +22,37 @@
 // - enfin de se remet en position initiale
 
 #include <Servo.h>
+// mini chef la bleue grande 
+//#define SLAVE_ADDRESS 0x13
+//#define SERVO_TETE_PIN    10
+//#define SERVO_TETE_MIN    600
+//#define SERVO_TETE_MAX    800
+//#define SERVO_PETALES_PIN 11
+//#define SERVO_PETALES_MIN 1600
+//#define SERVO_PETALES_MAX 2100
+//#define SERVO_TIGE_PIN    9
+//#define SERVO_TIGE_MIN    900
+//#define SERVO_TIGE_MAX    2300
+//#define CHORE_OUV_FER_NB  3
+//#define CHORE_ATTENTE_FIN 5000 
+
+// mini chef la bleue moyenne
+#define SLAVE_ADDRESS 0x13
+#define SERVO_TETE_PIN    10
+#define SERVO_TETE_MIN    600
+#define SERVO_TETE_MAX    800
+#define SERVO_PETALES_PIN 11
+#define SERVO_PETALES_MIN 800
+#define SERVO_PETALES_MAX 1500
+#define SERVO_TIGE_PIN    9
+#define SERVO_TIGE_INIT   1500
+#define SERVO_TIGE_MIN    900
+#define SERVO_TIGE_MAX    1500
+#define CHORE_OUV_FER_NB  3
+#define CHORE_ATTENTE_FIN 5000 
 
 // Led temoin
-int ledTemoin = 3; 
-         
-
+int ledTemoin = 3;
 
 // Servos
 Servo servoTete;  
@@ -65,13 +91,14 @@ void  setup()
  
 
   // Servo des petales qui s'ouvre et qui se ferme
-  servoPetales.attach(11);
+  servoPetales.attach(SERVO_PETALES_PIN);
   
   // Servo de la tete qui se penche vers la personne                
-  servoTete.attach(10);
+  servoTete.attach(SERVO_TETE_PIN);
 
   //servo de la tige qui se dirige vers les autres fleurs
-  servoTige.attach(9);
+  servoTige.attach(SERVO_TIGE_PIN);
+  servoTige.write(SERVO_TIGE_INIT);
 
   // Initialise la liaison série pour debug
   Serial.begin(9600);
@@ -83,30 +110,31 @@ void  loop()
     // Execute le programme :
     // - elle se dirige vers la personne
     Serial.println("Révérence");
-    parcourtServo(servoTete, 1000, 1000, 5);
-    parcourtServo(servoPetales, 1800, 1200, 5);
+    parcourtServo(servoTete, SERVO_TETE_MIN, SERVO_TETE_MAX, 5);
+    parcourtServo(servoPetales, SERVO_PETALES_MAX, SERVO_PETALES_MIN, 5);
       
     
     
     // - elle s'ouvre et se ferme 3x 
     for (int i = 0; i < 3; i++) {
       Serial.println("Ouverture pétales");
-      parcourtServo(servoPetales, 900, 1800, 5);
+      parcourtServo(servoPetales, SERVO_PETALES_MIN, SERVO_PETALES_MAX, 5);
       Serial.println("Fermeture pétales");
-      parcourtServo(servoPetales, 1800, 900, 2);
+      parcourtServo(servoPetales, SERVO_PETALES_MAX, SERVO_PETALES_MIN, 2);
     }
-  // - elle passe en revu toute les autres fleurs de droite a gauche
-      Serial.println("se dirige vers le premier de file");
-      parcourtServo(servoTige, 1550, 900, 5);
-      Serial.println("commence l'inspection jusqu'a l'autre bout");
+     parcourtServo(servoTige, SERVO_TIGE_INIT, SERVO_TIGE_MAX, 5);
+     parcourtServo(servoTige, SERVO_TIGE_MAX, SERVO_TIGE_MIN, 5);
+     
+     parcourtServo(servoTige, SERVO_TIGE_MIN, SERVO_TIGE_INIT, 5);
+  
 
      
       
     
     // - puis elle se remet en position initiale
     Serial.println("Retour en position initiale");
-    parcourtServo(servoTete, 1000, 1000, 5);
-    parcourtServo(servoPetales, 1200, 1800, 5);
+    parcourtServo(servoTete, SERVO_TETE_MAX, SERVO_TETE_MIN, 5);
+    parcourtServo(servoPetales, SERVO_PETALES_MIN, SERVO_PETALES_MAX, 5);
    
 
     // - Attente pour s'économiser un peu qd qq active le capteur
